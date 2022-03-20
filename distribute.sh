e3911181299da5a3d8a8965ec153a069aa205b04
José Benavente <jose@bgmp.cl>
2022-03-20 19:52:08 -0300

    Add a distribution shell script

---
+++ distribute.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+readonly DIST=/home/deploy/distribute
+
+bundle exec rake
+
+[ -e $DIST/rva/rva_cars.zip ] && rm -r $DIST/rva/rva_cars.zip
+[ -e $DIST/rva/rva_tracks.zip ] && rm -r $DIST/rva/rva_tracks.zip
+[ -e $DIST/rva/packages.json ] && rm $DIST/packages.json
+
+mv rva_cars/rva_cars.zip $DIST/rva
+mv rva_tracks/rva_tracks.zip $DIST/rva
+mv packages.json $DIST/packages.json
