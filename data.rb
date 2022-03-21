a12de58f7f69211f4417b946f0fce86699d02f58
José Benavente <jose@bgmp.cl>
2022-03-20 21:44:34 -0300

    Update package data

--- data.rb
+++ data.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module RVA
-  NAME = 'rva'
+  NAME = 'Re-Volt America'
   DESCRIPTION = "Re-Volt America's Official Content Pack"
   YEAR = 22
   MONTH = 2
   DAY = 21
-  VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}"
+  VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}"
 end
