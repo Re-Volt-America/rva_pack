19a7bb8dde21c6bb47979f93107345b7a2802928
José Benavente <jose@bgm.cl>
2022-12-14 00:00:11 -0300

    Update repository version

--- data.rb
+++ data.rb
@@ -4,8 +4,8 @@ module RVA
   NAME = 'Re-Volt America'
   DESCRIPTION = "Re-Volt America's Official Content Pack"
   YEAR = 22
-  MONTH = 5
-  DAY = 9
+  MONTH = 12
+  DAY = 13
   REVISION = 1
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}#{SUFFIX}-#{REVISION}"
