8bcd37585c53cc90c6339483e5accf52051cef13
José Benavente <jose@bgm.cl>
2024-02-02 12:11:54 -0300

    Bump revisions

--- data.rb
+++ data.rb
@@ -16,7 +16,7 @@ module RVA
     YEAR = 23
     MONTH = 10
     DAY = 30
-    REVISION = 1
+    REVISION = 2
     SUFFIX = 'a'
     VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
     URL = 'https://distribute.rva.lat/rva/rva_cars.zip'
@@ -28,7 +28,7 @@ module RVA
     YEAR = 23
     MONTH = 10
     DAY = 30
-    REVISION = 1
+    REVISION = 2
     SUFFIX = 'a'
     VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
     URL = 'https://distribute.rva.lat/rva/rva_tracks.zip'
