e082ad73c45ae26cde5880c3b87fa2872bb0cea0
BGMP <jose@bgm.cl>
2024-08-08 22:59:44 -0400

    Update to version 24.0501a-2

--- data.rb
+++ data.rb
@@ -6,7 +6,7 @@ module RVA
   YEAR = 24
   MONTH = 5
   DAY = 1
-  REVISION = 1
+  REVISION = 2
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
 
@@ -14,8 +14,8 @@ module RVA
     NAME = 'rva_cars'
     DESCRIPTION = "Re-Volt America's Cars Pack"
     YEAR = 24
-    MONTH = 5
-    DAY = 1
+    MONTH = 8
+    DAY = 8
     REVISION = 1
     SUFFIX = 'a'
     VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
@@ -26,8 +26,8 @@ module RVA
     NAME = 'rva_tracks'
     DESCRIPTION = "Re-Volt America's Tracks Pack"
     YEAR = 24
-    MONTH = 5
-    DAY = 1
+    MONTH = 8
+    DAY = 8
     REVISION = 1
     SUFFIX = 'a'
     VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
