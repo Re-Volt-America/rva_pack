97a4fa2fffe183d01abc216a7772f6ab717f600c
EstebanMz <estebanmz@rva.lat>
2025-08-25 13:44:34 -0500

    Update to version 25.0825a-1

--- data.rb
+++ data.rb
@@ -4,9 +4,9 @@ module RVA
   NAME = 'Re-Volt America'
   DESCRIPTION = "Re-Volt America's Official Content Pack"
   YEAR = 25
-  MONTH = 1
-  DAY = 5
-  REVISION = 3
+  MONTH = 8
+  DAY = 25
+  REVISION = 1
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
 
@@ -14,8 +14,8 @@ module RVA
     NAME = 'rva_cars'
     DESCRIPTION = "Re-Volt America's Cars Pack"
     YEAR = 25
-    MONTH = 2
-    DAY = 4
+    MONTH = 8
+    DAY = 25
     REVISION = 1
     SUFFIX = 'a'
     VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
@@ -26,8 +26,8 @@ module RVA
     NAME = 'rva_tracks'
     DESCRIPTION = "Re-Volt America's Tracks Pack"
     YEAR = 25
-    MONTH = 4
-    DAY = 14
+    MONTH = 8
+    DAY = 25
     REVISION = 1
     SUFFIX = 'a'
     VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}#{SUFFIX}-#{REVISION}"
